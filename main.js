7bd39824d1795736420ba98391068a5f6b09d80a
Laurentiu <lcostin40@gmail.com>
2021-10-27 22:05:54 +0200

    Aggiunti commenti e modificato nomi di alcuni array

--- main.js
+++ main.js
@@ -1,41 +1,69 @@
-
+//Array vuoto in cui finiranno i numeri casuali
 let numeriRandom = [];
 
+//For che genera fino a 5 numeri casuali
 for (i = 0; i < 5; i++) {
 
+    //Assegna ad una costante i numeri casuali
     let numeri = Math.floor(Math.random() * 100);
 
+    //Inseriche nell'array vuoto i numeri casuali
     numeriRandom.push(numeri);
 
 
 }
 
-console.log(numeriRandom);
+/* console.log(numeriRandom); */
 
-alert("I numeri casuali da ricordare sono: " + numeriRandom);
+//Messaggio che leggerà l'utente
+alert("I numeri casuali da ricordare sono: " + numeriRandom + ". Dopo aver premuto ok avrai 30 secondi per far mente locale ed inserire i numeri");
 
-const risposte = [];
 
-for (i = 0; i < 5; i++) {
 
-    let risposta = Number(prompt("Inserisci i numeri"));
 
-    risposte.push(risposta)
-}
 
-console.log(risposte);
 
-const pointsScored = []
+//Funzione che ritarda la possibilità di rispondere di 30 secondi
+setTimeout(time, 30000)
 
-for (i = 0; i < risposte.length; i++) {
+//Funzione che dà la possibilità di rispondere
+function time() {
 
-    const rispostaa = risposte[i]
+    //Array vuoto in cui finiranno i numeri inseriti dall'utente
+    const numeriScelti = [];
 
-    if (numeriRandom.includes(rispostaa)) {
-        pointsScored.push(rispostaa)
+    //For che chiederà per 5 volte di inserire un numero
+    for (i = 0; i < 5; i++) {
+
+        //Aggiunge ad una costante il numero scelto dall'utente
+        let numeroScelto = Number(prompt("Inserisci i numeri"));
+
+        //Inserice nell'array vuoto i numeri scelti dall'utente
+        numeriScelti.push(numeroScelto)
     }
+
+    /* console.log(numeriScelti); */
+
+    //Array vuoto in cui finiranno i numeri esatti scelti dall'utente
+    const numeriEsatti = []
+
+    //For che fa la verifica dei numeri scelti dall'utente
+    for (i = 0; i < numeriScelti.length; i++) {
+
+        //array che contiene il numero giusto
+        const numeroEsatto = numeriScelti[i]
+
+        //If che verifica i numeri 
+        if (numeriRandom.includes(numeroEsatto)) {
+
+            //Aggiunge il numero azzeccato all'array vuoto
+            numeriEsatti.push(numeroEsatto)
+        }
+    }
+
+
+
+    //Alert che fa mostrare quanti e quali numeri l'utente si è ricordato
+    alert("I numeri che sei riuscito a ricordare sono: " + numeriEsatti);
+
 }
-
-
-
-console.log("I numeri che ti sei ricordati sono: " + pointsScored);
