df4cab9f7a177f56b7db8f662f3a30bf94da6bc6
Laurentiu <lcostin40@gmail.com>
2021-10-27 21:34:39 +0200

    Aggiunto codice js per la verifica dei numeri inseriti

--- main.js
+++ main.js
@@ -6,8 +6,36 @@ for (i = 0; i < 5; i++) {
     let numeri = Math.floor(Math.random() * 100);
 
     numeriRandom.push(numeri);
+
+
 }
 
 console.log(numeriRandom);
 
 alert("I numeri casuali da ricordare sono: " + numeriRandom);
+
+const risposte = [];
+
+for (i = 0; i < 5; i++) {
+
+    let risposta = Number(prompt("Inserisci i numeri"));
+
+    risposte.push(risposta)
+}
+
+console.log(risposte);
+
+const pointsScored = []
+
+for (i = 0; i < risposte.length; i++) {
+
+    const rispostaa = risposte[i]
+
+    if (numeriRandom.includes(rispostaa)) {
+        pointsScored.push(rispostaa)
+    }
+}
+
+
+
+console.log("I numeri che ti sei ricordati sono: " + pointsScored);
